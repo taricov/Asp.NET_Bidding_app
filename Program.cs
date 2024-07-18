2ab627c604f5edf20a168b901996fbb4bd7fb5ea
TaricOv <tareq.ouf@gmail.com>
2024-07-18 21:35:11 +0300

    working on the ListingView . added the LisitngService to the program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using BidingApp.Data;
+using BidingApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IListingService, ListingService>();
 
 var app = builder.Build();
 
